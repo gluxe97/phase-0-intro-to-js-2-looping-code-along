f6446d4f2945f04fbe054e3aa2cf1ef174ab42a5
Gabriel <dre2059570@gmail.com>
2022-01-06 04:36:00 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+    let message=[];
+    for(let i =0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }           
+  return message;
+};
+writeCards(["Gabriel", "Gonzo", "Rob", "Pedro"], "birthday")
+
+function countDown(i){
+    while((i+1)>0) {
+        console.log(i);
+        (i--);
+    }
+}
+
